3b87ff4a011b1e8fc3adf22e0e33322cddf96eee
ghislaindentremont <ghislaindentremont@gmail.com>
2016-11-29 13:44:04 -0400

    Butterworth filter added
    TO BE TESTED

--- NFT_experiment.m
+++ NFT_experiment.m
@@ -3,195 +3,211 @@ sca;
 close all;
 clearvars;
 
-% Here we call some default settings for setting up Psychtoolbox
-PsychDefaultSetup(2);
-
-% Get the screen numbers
-screens = Screen('Screens');
-
-% Draw to the external screen if avaliable
-screen_number = max(screens);
-
-% Define black and white
-white = WhiteIndex(screen_number);
-black = BlackIndex(screen_number);
-
-% Open an on screen window
-[window, window_rect] = PsychImaging('OpenWindow', screen_number, black);
-% HideCursor(); 
-
-% Get the size of the on screen window
-[screenXpixels, screenYpixels] = Screen('WindowSize', window);
-
-% Query the frame duration
-ifi = Screen('GetFlipInterval', window);
-
-% Query the maximum priority level
-top_priority_level = MaxPriority(window);  % I may not make use of this below.
-
-% Get the centre coordinate of the window
-[xCenter, yCenter] = RectCenter(window_rect);
-
-% Set up alpha-blending for smooth (anti-aliased) lines
-Screen('BlendFunction', window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
-
-
-
-%----------------------------------------------------------------------
-%                       Fixation Cross 
-%----------------------------------------------------------------------
-
-% Here we set the size of the arms of our fixation cross
-fix_cross_dim_pix = 40*2;
-
-% Now we set the coordinates (these are all relative to zero we will let
-% the drawing routine center the cross in the center of our monitor for us)
-x_fix_coords = [-fix_cross_dim_pix fix_cross_dim_pix 0 0];
-y_fix_coords = [0 0 -fix_cross_dim_pix fix_cross_dim_pix];
-all_fix_coords = [x_fix_coords; y_fix_coords];
-
-% Set the line width for our fixation cross
-line_width_pix = 4;
-
-% make it green 
-fix_color = [0 1 0];
-
-% just vertical line
-all_cue_coords = [[0 0 0 0]; y_fix_coords];
-
-
-
-%----------------------------------------------------------------------
-%                         Arrow Cue 
-%----------------------------------------------------------------------
-
-% arrow color - red 
-arr_color = [1 0 0];
-
-% draw right arrow 
-rarrow_base = [xCenter + line_width_pix/2
-    , yCenter - fix_cross_dim_pix/2
-    , xCenter + line_width_pix/2 + fix_cross_dim_pix
-    , yCenter + fix_cross_dim_pix/2];
-
-rarrow_spear = [[xCenter + line_width_pix/2 + fix_cross_dim_pix, yCenter + fix_cross_dim_pix];
-    [xCenter + line_width_pix/2 + fix_cross_dim_pix*2, yCenter];
-    [xCenter + line_width_pix/2 + fix_cross_dim_pix, yCenter - fix_cross_dim_pix]];
-
-% draw right arrow 
-larrow_base = [xCenter - line_width_pix/2 - fix_cross_dim_pix
-    , yCenter - fix_cross_dim_pix/2
-    , xCenter - line_width_pix/2 
-    , yCenter + fix_cross_dim_pix/2];
-
-larrow_spear = [[xCenter - line_width_pix/2 - fix_cross_dim_pix, yCenter + fix_cross_dim_pix];
-    [xCenter - line_width_pix/2 - fix_cross_dim_pix*2, yCenter];
-    [xCenter - line_width_pix/2 - fix_cross_dim_pix, yCenter - fix_cross_dim_pix]];
-
-
-
-%----------------------------------------------------------------------
-%                         NF Bar 
-%----------------------------------------------------------------------
-
-NF_color = [0 0 1];
-
-
-
-%----------------------------------------------------------------------
-%                       Timing Information
-%----------------------------------------------------------------------
-
-% Baseline duration
-REST_TIME = 2;
-
-% Fixation interval time in seconds and frames
-fix_time = 2;
-fix_time_frames = round(fix_time / ifi);
-
-% Cue interval time in seconds and frames 
-cue_time = 1.25;
-cue_time_frames = round(cue_time / ifi);
-
-% Numer of frames to wait before re-drawing
-wait_frames = 1;
-
-% NF time frames 
-NF_time = 5;
-NF_wait_frames = 3;
-NF_time_frames = round(NF_time / ifi / NF_wait_frames);
-
-
-
-
-%----------------------------------------------------------------------
-%                       Conditions
-%----------------------------------------------------------------------
-
-% cue location
-cue_locs_list = {'left', 'right'};
-cue_locs = [1, 2];
-
-trials_per_condition = 2;
-cond_matrix = repmat(cue_locs, 1, trials_per_condition);
-
-% Get the size of the matrix
-[~, num_trials] = size(cond_matrix);
-
-% Randomise the conditions
-shuffler = Shuffle(1:num_trials);
-cond_matrix_shuffled2 = cond_matrix(:, shuffler);
-
-% get ITIs 
-itis = rand(1,num_trials)+1;
-
-% final condition matrix
-cond_matrix_shuffled = [cond_matrix_shuffled2; itis];
-
-
-
-%----------------------------------------------------------------------
-%                       EEG Aquisition Setup
-%----------------------------------------------------------------------
-
-% should be equal to FS defined in next block
-sampling_rate = 128;
-
-MV_AVG_LENGTH = 1; % in seconds
-DATA_POINTS = MV_AVG_LENGTH * sampling_rate;
-
-PSD_FREQS = 13:1:30; 
-
-% FIGURE OUT MAPPINGS
-CHANNELS_OF_INTEREST = [8, 12]; % C3 = Ch8; C4 = Ch12
-
-data_buffer = zeros(length(CHANNELS_OF_INTEREST), DATA_POINTS); %pre-allocate data
-
-% make sure that everything is on the path and LSL is loaded
-disp('Loading the library...');
-lib = lsl_loadlib();
-
-% resolve a stream...
-disp('Resolving an EEG stream...');
-result = {};
-while isempty(result)
-    result = lsl_resolve_byprop(lib,'name','openvibeSignal'); end
-
-% create a new inlet
-disp('Opening an inlet...');
-inlet = lsl_inlet(result{1});
-
-% resolve stream sample rate
-FS = inlet.info.nominal_srate(); % apperently the sampling rate is 128 Hz
-
-
-
-
-%----------------------------------------------------------------------
-%                       Experimental Loop
-%----------------------------------------------------------------------
-
 try 
+
+    % Here we call some default settings for setting up Psychtoolbox
+    PsychDefaultSetup(2);
+
+    % Get the screen numbers
+    screens = Screen('Screens');
+
+    % Draw to the external screen if avaliable
+    screen_number = max(screens);
+
+    % Define black and white
+    white = WhiteIndex(screen_number);
+    black = BlackIndex(screen_number);
+
+    % Open an on screen window
+    [window, window_rect] = PsychImaging('OpenWindow', screen_number, black);
+    % HideCursor(); 
+
+    % Get the size of the on screen window
+    [screenXpixels, screenYpixels] = Screen('WindowSize', window);
+
+    % Query the frame duration
+    ifi = Screen('GetFlipInterval', window);
+
+    % Query the maximum priority level
+    top_priority_level = MaxPriority(window);  % I may not make use of this below.
+
+    % Get the centre coordinate of the window
+    [xCenter, yCenter] = RectCenter(window_rect);
+
+    % Set up alpha-blending for smooth (anti-aliased) lines
+    Screen('BlendFunction', window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
+
+
+
+    %----------------------------------------------------------------------
+    %                       Fixation Cross 
+    %----------------------------------------------------------------------
+
+    % Here we set the size of the arms of our fixation cross
+    fix_cross_dim_pix = 40*2;
+
+    % Now we set the coordinates (these are all relative to zero we will let
+    % the drawing routine center the cross in the center of our monitor for us)
+    x_fix_coords = [-fix_cross_dim_pix fix_cross_dim_pix 0 0];
+    y_fix_coords = [0 0 -fix_cross_dim_pix fix_cross_dim_pix];
+    all_fix_coords = [x_fix_coords; y_fix_coords];
+
+    % Set the line width for our fixation cross
+    line_width_pix = 4;
+
+    % make it green 
+    fix_color = [0 1 0];
+
+    % just vertical line
+    all_cue_coords = [[0 0 0 0]; y_fix_coords];
+
+
+
+    %----------------------------------------------------------------------
+    %                         Arrow Cue 
+    %----------------------------------------------------------------------
+
+    % arrow color - red 
+    arr_color = [1 0 0];
+
+    % draw right arrow 
+    rarrow_base = [xCenter + line_width_pix/2
+        , yCenter - fix_cross_dim_pix/2
+        , xCenter + line_width_pix/2 + fix_cross_dim_pix
+        , yCenter + fix_cross_dim_pix/2];
+
+    rarrow_spear = [[xCenter + line_width_pix/2 + fix_cross_dim_pix, yCenter + fix_cross_dim_pix];
+        [xCenter + line_width_pix/2 + fix_cross_dim_pix*2, yCenter];
+        [xCenter + line_width_pix/2 + fix_cross_dim_pix, yCenter - fix_cross_dim_pix]];
+
+    % draw right arrow 
+    larrow_base = [xCenter - line_width_pix/2 - fix_cross_dim_pix
+        , yCenter - fix_cross_dim_pix/2
+        , xCenter - line_width_pix/2 
+        , yCenter + fix_cross_dim_pix/2];
+
+    larrow_spear = [[xCenter - line_width_pix/2 - fix_cross_dim_pix, yCenter + fix_cross_dim_pix];
+        [xCenter - line_width_pix/2 - fix_cross_dim_pix*2, yCenter];
+        [xCenter - line_width_pix/2 - fix_cross_dim_pix, yCenter - fix_cross_dim_pix]];
+
+
+
+    %----------------------------------------------------------------------
+    %                         NF Bar 
+    %----------------------------------------------------------------------
+
+    NF_color = [0 0 1];
+
+
+
+    %----------------------------------------------------------------------
+    %                       Timing Information
+    %----------------------------------------------------------------------
+
+    % Fixation interval time in seconds and frames
+    fix_time = 2;
+    fix_time_frames = round(fix_time / ifi);
+
+    % Cue interval time in seconds and frames 
+    cue_time = 1.25;
+    cue_time_frames = round(cue_time / ifi);
+
+    % Numer of frames to wait before re-drawing
+    wait_frames = 1;
+
+    % NF time frames 
+    NF_time = 5;
+    NF_wait_frames = 3;
+    NF_time_frames = round(NF_time / ifi / NF_wait_frames);
+
+    % Baseline duration
+    baseline_time = 2;
+    baseline_time_frames = round(baseline_time / ifi / NF_wait_frames);
+
+
+
+    %----------------------------------------------------------------------
+    %                       Conditions
+    %----------------------------------------------------------------------
+
+    % cue location
+    cue_locs_list = {'left', 'right'};
+    cue_locs = [1, 2];
+
+    trials_per_condition = 2;
+    cond_matrix = repmat(cue_locs, 1, trials_per_condition);
+
+    % Get the size of the matrix
+    [~, num_trials] = size(cond_matrix);
+
+    % Randomise the conditions
+    shuffler = Shuffle(1:num_trials);
+    cond_matrix_shuffled2 = cond_matrix(:, shuffler);
+
+    % get ITIs 
+    itis = rand(1,num_trials)+1;
+
+    % final condition matrix
+    cond_matrix_shuffled = [cond_matrix_shuffled2; itis];
+
+
+
+    %----------------------------------------------------------------------
+    %                       EEG Aquisition Setup
+    %----------------------------------------------------------------------
+
+    % should be equal to FS defined in next block
+    sampling_rate = 128;
+
+    MV_AVG_LENGTH = 1; % in seconds
+    DATA_POINTS = MV_AVG_LENGTH * sampling_rate;
+
+    PSD_FREQS = 13:1:30; 
+
+    % FIGURE OUT MAPPINGS
+    CHANNELS_OF_INTEREST = [8, 12]; % C3 = Ch8; C4 = Ch12
+
+    data_buffer = zeros(length(CHANNELS_OF_INTEREST), DATA_POINTS); %pre-allocate data
+
+    % make sure that everything is on the path and LSL is loaded
+    disp('Loading the library...');
+    lib = lsl_loadlib();
+
+    % resolve a stream...
+    disp('Resolving an EEG stream...');
+    result = {};
+    while isempty(result)
+        result = lsl_resolve_byprop(lib,'name','openvibeSignal'); end
+
+    % create a new inlet
+    disp('Opening an inlet...');
+    inlet = lsl_inlet(result{1});
+
+    % resolve stream sample rate
+    FS = inlet.info.nominal_srate(); % apperently the sampling rate is 128 Hz
+
+    
+    %----------------------------------------------------------------------
+    %                            Filters
+    %----------------------------------------------------------------------
+    
+    lo = 4;  % keeps 13 Hz ~0 dB
+    hi = 60;
+    
+    % 2nd order (2n, where n is first argument)
+    % second argument must be between 0 and 1 (Nyquist - FS/2)
+    [b, a] = butter(1,[lo hi]/(FS/2), 'bandpass');
+%     fvtool(b, a);
+
+    
+
+
+
+    %----------------------------------------------------------------------
+    %                       Experimental Loop
+    %----------------------------------------------------------------------
+
+
     for trial = 1:num_trials
 
         %---------------- Baseline EEG Aquisition -------------------------
@@ -211,8 +227,10 @@ try
 
             disp('Resting EEG Aquisition...')
             Screen('Flip', window);
-            tic; 
-            while toc < REST_TIME
+            
+            % Flip to the screen
+            for frame = 1:baseline_time_frames-1
+                
                 [temp_data, ts] = inlet.pull_chunk();
 
                 new_points = temp_data(CHANNELS_OF_INTEREST, :);
@@ -221,15 +239,21 @@ try
                 data_buffer(:,1:DATA_POINTS-new_length) = data_buffer(:,new_length+1:end);
                 data_buffer(:,DATA_POINTS-new_length+1:end) = new_points;
 
-                display_buffer = detrend(data_buffer.');
+%                 display_buffer = detrend(data_buffer.');
+                data_buffer = data_buffer.';
+                display_buffer = filter(b,a,data_buffer);
 
                 [Pxx, Fxx] = pwelch(display_buffer, [], [], PSD_FREQS, FS, 'power');
                 
-                 power_rest_list = [power_rest_list, mean(mean(Pxx))]; 
+                power_rest_list = [power_rest_list, mean(mean(Pxx))]; 
+                
+                                        % Flip to the screen
+                vbl = Screen('Flip', window, vbl + (NF_wait_frames - 0.5) * ifi);
 
             end
-            power_rest = mean(power_rest_list); 
 
+            power_rest = mean(power_rest_list); 
+           
         end
         %----------------------------------------------------------------------
 
@@ -327,7 +351,9 @@ try
         % detrend removes the mean value or linear trend from a vector or matrix, usually for FFT processing
         % it basically just transposes to zero - you can see this by comparing
         % graphs
-        display_buffer = detrend(data_buffer.');
+%         display_buffer = detrend(data_buffer.');
+        data_buffer = data_buffer.';
+        display_buffer = filter(b,a,data_buffer);
         
         disp('NEED TO CONFIRM DIRECTIONALITY OF LI')
         [Pxx, Fxx] = pwelch(display_buffer, [], [], PSD_FREQS, FS, 'power');
